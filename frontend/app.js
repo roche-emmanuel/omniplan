0fc742e2b3b4e0f98b9d0fc997d88b8b2cec0208
manu <roche.emmanuel@gmail.com>
2015-08-30 16:23:07 +0200

    Working on issue #1: Added some update on the display of the posts.

--- frontend/app.js
+++ frontend/app.js
@@ -3,5 +3,13 @@ var app = angular.module('omniPlan', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
+  ];
+  
   $scope.test = 'Hello world!';
 }]);
