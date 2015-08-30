750beac96d6131be1b5a432d684b1aaea7def442
manu <roche.emmanuel@gmail.com>
2015-08-30 16:30:52 +0200

    Added form to porvide title when posting new post.

--- frontend/app.js
+++ frontend/app.js
@@ -12,6 +12,8 @@ function($scope){
   ];
 
   $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
   };
 }]);
