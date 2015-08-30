4c322f547820f7c374ef7a6775697b883623aaf7
manu <roche.emmanuel@gmail.com>
2015-08-30 17:13:54 +0200

    Working on issue #1: Added support for ui-router usage.

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,19 @@
-var app = angular.module('omniPlan', []);
+var app = angular.module('omniPlan', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function(){
   var o = {
