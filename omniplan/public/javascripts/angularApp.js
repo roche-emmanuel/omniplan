2b29e8cdbe8548b7db4e20c7e547db4ebdfb2930
manu <roche.emmanuel@gmail.com>
2015-08-31 21:45:06 +0200

    Fixed issue #1: wiring frontend completely for simple post app template.

--- omniplan/public/javascripts/angularApp.js
+++ omniplan/public/javascripts/angularApp.js
@@ -19,7 +19,12 @@ function($stateProvider, $urlRouterProvider) {
     .state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
-      controller: 'PostsCtrl'
+      controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
     });
 
   $urlRouterProvider.otherwise('home');
@@ -34,6 +39,36 @@ app.factory('posts', ['$http',function($http){
     return $http.get('/posts').success(function(data){
       angular.copy(data, o.posts);
     });
+  };
+
+  o.create = function(post) {
+    return $http.post('/posts', post).success(function(data){
+      o.posts.push(data);
+    });
+  };
+
+  o.upvote = function(post) {
+    return $http.put('/posts/' + post._id + '/upvote')
+      .success(function(data){
+        post.upvotes += 1;
+      });
+  };
+
+  o.get = function(id) {
+    return $http.get('/posts/' + id).then(function(res){
+      return res.data;
+    });
+  };
+
+  o.addComment = function(id, comment) {
+    return $http.post('/posts/' + id + '/comments', comment);
+  };
+
+  o.upvoteComment = function(post, comment) {
+    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+      .success(function(data){
+        comment.upvotes += 1;
+      });
   };
 
   return o;
@@ -53,36 +88,50 @@ function($scope,posts){
 
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({
+    
+    posts.create({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0,
-      comments: [
-        {author: 'Joe', body: 'Cool post!', upvotes: 0},
-        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-      ]
     });
+
+    // $scope.posts.push({
+    //   title: $scope.title,
+    //   link: $scope.link,
+    //   upvotes: 0,
+    //   comments: [
+    //     {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    //     {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+    //   ]
+    // });
     $scope.title = '';
     $scope.link = '';
   };
 
   $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
+    posts.upvote(post);
+    // post.upvotes += 1;
   };
 }]);
 
 app.controller('PostsCtrl', [
-'$scope','$stateParams','posts',
-function($scope,$stateParams,posts){
-  $scope.post = posts.posts[$stateParams.id];
+'$scope','posts','post',
+function($scope,posts,post){
+  // $scope.post = posts.posts[$stateParams.id];
+  $scope.post = post;
 
   $scope.addComment = function(){
   if($scope.body === '') { return; }
-  $scope.post.comments.push({
+  posts.addComment(post._id, {
     body: $scope.body,
     author: 'user',
-    upvotes: 0
+  }).success(function(comment) {
+    $scope.post.comments.push(comment);
   });
+
+  $scope.incrementUpvotes = function(comment){
+    posts.upvoteComment(post, comment);
+  };
+
   $scope.body = '';
 };
 }]);
